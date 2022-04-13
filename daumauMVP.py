73506d52c713ea3858f460d14a922a0767ba2995
Giovanni Zanatta <giovanni.zanatta@rankmyapp.com.br>
2022-04-13 16:31:16 -0300

    first commit

--- daumauMVP.py
+++ daumauMVP.py
@@ -6,8 +6,8 @@ from matplotlib.legend_handler import HandlerLine2D
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Imagem
-#img = Image.open('logo_daumau.png')
-#st.image(img)
+img = Image.open('logo_daumau.png')
+st.image(img)
 
 # Título
 st.markdown('## Validação de estimativas para DAU/MAU/Stickiness - MVF')
